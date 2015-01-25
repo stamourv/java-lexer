c3f629ebc453047e0539cb7b158d7d78e558359b
Vincent St-Amour <stamourv@racket-lang.org>
2015-01-25 10:29:37 -0500

    Fix collection name.

--- main.rkt
+++ main.rkt
@@ -6,6 +6,6 @@
 
 ;; from Ben Lerner. depends on the `java` collection, which wraps profj stuff
 (define-syntax-rule (java-block arg args ...)
-  (codeblock #:keep-lang-line? #f #:context #'arg "#lang java\n" arg args ...))
+  (codeblock #:keep-lang-line? #f #:context #'arg "#lang java-lexer\n" arg args ...))
 (define-syntax-rule (java arg args ...)
-  (make-element (make-style "RktBlk" '(tt-chars)) (code #:lang "java" arg args ...)))
+  (make-element (make-style "RktBlk" '(tt-chars)) (code #:lang "java-lexer" arg args ...)))
